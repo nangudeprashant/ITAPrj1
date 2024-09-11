b7560fd88abee73cbc238d781a6a7d0224b1dff2
Admin <Admin@CONSISTENT>
2024-09-11 20:28:26 +0530

    Initial commit

---
+++ ITAPrj1/src/main/java/itaprj1/com/App.java
@@ -0,0 +1,13 @@
+package itaprj1.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
